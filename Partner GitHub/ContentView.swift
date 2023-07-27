d5da41af1a1547e8a882e8b72ff245d4877fd140
Ila <iferrier2025@dominicanacademy.org>
2023-07-27 13:15:43 -0400

    initial comment

--- Partner GitHub/ContentView.swift
+++ Partner GitHub/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, earth!")
+            Text("My name is Ila")
         }
         .padding()
     }
