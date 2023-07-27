09ca6aa5ef2a47b9300e6e85c228d0242fdc35ec
Ila <iferrier2025@dominicanacademy.org>
2023-07-27 13:12:38 -0400

    Initial Commit

---
+++ Partner GitHub/Partner_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Partner_GitHubApp.swift
+//  Partner GitHub
+//
+//  Created by scholar on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Partner_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
